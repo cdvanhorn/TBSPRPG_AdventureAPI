95ce9e6f6f7bdb95411fdc9429802acee2de016a
cdvanhorn <cdavid.vanhorn@gmail.com>
2020-12-17 20:18:36 -0500

    AdventureAPI
    More code standardization.

--- AdventureApi/Repositories/AdventuresRepository.cs
+++ AdventureApi/Repositories/AdventuresRepository.cs
@@ -6,7 +6,7 @@ using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 
-using AdventureApi.Utilities;
+using TbspRpgLib.Settings;
 
 namespace AdventureApi.Repositories {
     public interface IAdventuresRepository {
--- AdventureApi/Startup.cs
+++ AdventureApi/Startup.cs
@@ -1,6 +1,3 @@
-using AdventureApi.Repositories;
-using AdventureApi.Services;
-
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -15,7 +12,12 @@ using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 
-using AdventureApi.Utilities;
+using TbspRpgLib.Jwt;
+using TbspRpgLib.Settings;
+using TbspRpgLib.Events;
+
+using AdventureApi.Services;
+using AdventureApi.Repositories;
 
 namespace AdventureApi
 {
@@ -32,12 +34,24 @@ namespace AdventureApi
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddScoped<IEventService, EventService>();
             services.AddScoped<IAdventuresService, AdventuresService>();
             services.AddScoped<IAdventuresRepository, AdventuresRepository>();
 
             services.Configure<DatabaseSettings>(Configuration.GetSection("Database"));
             services.AddSingleton<IDatabaseSettings>(sp =>
                 sp.GetRequiredService<IOptions<DatabaseSettings>>().Value);
+                
+            services.Configure<JwtSettings>(Configuration.GetSection("JwtSettings"));
+            services.AddSingleton<IJwtSettings>(sp =>
+                sp.GetRequiredService<IOptions<JwtSettings>>().Value);
+            
+            services.Configure<EventStoreSettings>(Configuration.GetSection("EventStore"));
+            services.AddSingleton<IEventStoreSettings>(sp =>
+                sp.GetRequiredService<IOptions<EventStoreSettings>>().Value);
+
+            //start workers
+            //services.AddHostedService<MyNewGameEventProcessor>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -51,6 +65,8 @@ namespace AdventureApi
             app.UseHttpsRedirection();
 
             app.UseRouting();
+
+            app.UseMiddleware<JwtMiddleware>();
 
             //app.UseAuthorization();
 
