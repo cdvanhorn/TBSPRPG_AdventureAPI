77ef6c5f0b2ce78fe8dab84361141d80177ecc81
cdvanhorn <cdavid.vanhorn@gmail.com>
2021-01-05 18:09:07 -0500

    MapAPI
    Removed class that I created when I was frustrated.

--- AdventureApi/Entities/LocationFu.cs
+++
@@ -1,9 +0,0 @@
-namespace AdventureApi.Entities {
-    public class LocationFu {
-        public int Id { get; set; }
-
-        public int AdventureId { get; set; }
-    
-        public bool Initial { get; set; }
-    }
-}
